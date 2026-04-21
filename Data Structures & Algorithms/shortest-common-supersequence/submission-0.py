ac4bcd1cbc9de0f6e103142d11834da5524ca49e
Michael JK <michjk@users.noreply.github.com>
2026-04-21 13:57:54 +0800

    Add: shortest-common-supersequence - submission-0

---
+++ Data Structures & Algorithms/shortest-common-supersequence/submission-0.py
@@ -0,0 +1,42 @@
+class Solution:
+    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
+        m = len(str1)
+        n = len(str2)
+
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(m + 1):
+            for j in range(n + 1):
+                if i == 0:
+                    dp[i][j] = j
+                elif j == 0:
+                    dp[i][j] = i
+                elif str1[i - 1] == str2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1])
+        
+        res = []
+        i, j = m, n
+
+        while i > 0 and j > 0:
+            if str1[i - 1] == str2[j - 1]:
+                res.append(str1[i - 1])
+                i -= 1
+                j -= 1
+            elif dp[i - 1][j] < dp[i][j - 1]:
+                res.append(str1[i - 1])
+                i -= 1
+            else:
+                res.append(str2[j - 1])
+                j -= 1
+        
+        while i > 0:
+            res.append(str1[i - 1])
+            i -= 1
+
+        while j > 0:
+            res.append(str2[j - 1])
+            j -= 1
+        
+        return ''.join(reversed(res))
